4d9f81d533d294d10e625592d5620e609fd04757
Aguinaldo Brito <aguinaldobrito@MacBook-Air-de-Aguinaldo.local>
2020-12-10 12:29:41 -0300

    teste: arquivos do treinamento Agilizei02

---
+++ cypress/integration/compra.spec.js
@@ -0,0 +1,70 @@
+/// <reference types="cypress"/>
+
+context('Compra', () => {
+    it('Efetuar uma compra de Produto', () => {
+        // http://automationpractice.com/index.php
+        cy.visit('/');
+
+        let nomeProduto = 'Faded Short Sleeve T-shirts';
+
+        cy.contains(nomeProduto).trigger('mouseover')
+
+        cy.contains(nomeProduto)
+        .parent()
+        .siblings('div.button-container')
+        .children ('a')
+        .first()
+        .click()
+
+    cy.get ('.icon-ok')
+    .parent()
+    .should('contain.text', 'Product successfully added to your shopping cart')
+
+    cy.get ('span#layer_cart_product_title').should('contain.text', nomeProduto)
+
+    cy.get (".button-container a[href$='controller=order']").click()
+
+    cy.get (".cart_navigation a[href$='order&step=1']").click()
+
+    cy.get ('#email').type('thomas.shelby@gmail.com')
+    cy.get ('#passwd').type('teste123')
+
+    cy.get ('#SubmitLogin').click()
+
+    cy.get ('[type=checkbox]#addressesAreEquals').should('have.attr', 'checked', 'checked')
+    cy.get ('[type=checkbox]#addressesAreEquals').should('have.attr', 'name', 'same')
+
+    cy.get ('[name=processAddress]').click()
+
+    cy.get ('[type=checkbox]#cgv').check()
+
+    cy.get ('button[name=processCarrier]').click()
+
+    cy.get ('.bankwire').click()
+
+    cy.get ('.cart_navigation button[type=submit]')
+        .find ('span')
+        .contains ('I confirm my order')
+        .click()
+
+    cy.get ('.cheque-indent strong')
+        .should ('contain.text', 'Your order on My Store is complete.')
+
+    cy.get ('div.box').invoke('text').then((text) => {
+        console.log(text)
+
+        console.log(text.match(/[A-Z][A-Z]+/g)[1])
+
+        cy.writeFile('cypress/fixtures/pedido.json', {id: `${ text.match(/[A-Z][A-Z]+/g)[1] }` })
+    })
+
+    cy.get (".cart_navigation a[href$='history']").click()
+
+    cy.readFile('cypress/fixtures/pedido.json').then((pedido) => {
+
+        cy.get ('tr.first_item .history_link a').should('contain.text', pedido.id)
+    })
+
+});
+
+});
